66494ff709e021caa78a2ba3540880c9500b536f
vgawade-spurqlabs1 <vaibhav.gawade@spurqlabs.com>
2026-06-01 11:35:05 +0530

    Devops Tool

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+    jest: true
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "script"
+  },
+  rules: {
+    "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+  }
+};
